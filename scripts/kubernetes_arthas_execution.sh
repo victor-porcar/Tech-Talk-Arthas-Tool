232714f8f82f703a76d64fb0d55a7fb5c2234027
Víctor Porcar <56938399+victor-porcar@users.noreply.github.com>
2024-07-02 11:47:39 +0200

    Update kubernetes_arthas_execution.sh

--- scripts/kubernetes_arthas_execution.sh
+++ scripts/kubernetes_arthas_execution.sh
@@ -4,16 +4,16 @@ if [ "$#" -ne 3 ]; then
 	echo -e "\n"
     echo "Illegal number of parameters."
     echo "USAGE: arthas_execution_kubernetes.sh <KUBERNETES_NAMESPACE> <POD_NAME_PATTERN> <ARTHAS_COMMAND>"
-    echo "this command will apply the given <ARTHAS_COMMAND> to all pods having in its name the string <POD_NAME_PATTERN>"
+    echo "this command will apply the given <ARTHAS_COMMAND> to all pods belonging to the given Namespace and having in its name the string prefix <POD_NAME_PATTERN>"
     echo -e "\n"
-    echo 'Example: arthas_execution_kubernetes.sh "my-pod" "dashboard"'
+    echo 'Example: arthas_execution_kubernetes.sh "my-namespace" "my-pod" "dashboard"'
     echo -e "\n"
     exit;
 fi
 
-
-POD_NAME_PATTERN=$1
-ARTHAS_COMMAND=$2
+KUBERNETES_NAMESPACE=$1
+POD_NAME_PATTERN=$2
+ARTHAS_COMMAND=$3
 
 # this is the command that will be executed inside of the pod's shell to obtain 
 # java pid for the application to be applied arthas command
@@ -21,7 +21,7 @@ ARTHAS_COMMAND=$2
 PID_JAVA_SERVICE="ps -ef | grep -v 'grep java' | grep -v 'arthas-boot.jar' | grep java | awk '{print $"
 PID_JAVA_SERVICE="${PID_JAVA_SERVICE}1'}"
 
-POD_NAMES=$( kubectl get pods   | grep -P $POD_NAME_PATTERN | cut -d ' ' -f1 )
+POD_NAMES=$( kubectl get pods --namespace "$KUBERNETES_NAMESPACE" | grep -P "$POD_NAME_PATTERN" | cut -d ' ' -f1 )
 
 echo -e "\n"
 echo "The following Arthas command:"
